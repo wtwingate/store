fa2874c77eea21ced326308957f2c6b51aed0df8
William Wingate <wtwingate@gmail.com>
2025-09-04 16:35:50 -0400

    Add in-stock notifications for Products

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "products#index"
 
-  resources :products
+  resources :products do
+    resources :subscribers, only: [ :create ]
+  end
+
+  resource :unsubscribe, only: [ :show ]
 
   # The above is equivalent to:
 
