3d6bc0edd3fe1f260c9d93699987a43014f4d508
William Wingate <wtwingate@gmail.com>
2025-10-17 13:55:41 -0400

    Allow users to delete their account

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,7 @@ Rails.application.routes.draw do
   namespace :settings do
     resource :password, only: [ :show, :update ]
     resource :profile, only: [ :show, :update ]
+    resource :user, only: [ :show, :destroy ]
 
     root to: redirect("/settings/profile")
   end
