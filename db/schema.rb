183d89468ac6ecbf302bfcf06162a11d4114aa5e
William Wingate <wtwingate@gmail.com>
2025-10-17 14:53:40 -0400

    Add admin flag to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_17_175837) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_17_184709) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -81,6 +81,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_17_175837) do
     t.string "first_name"
     t.string "last_name"
     t.string "unconfirmed_email"
+    t.boolean "admin"
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
