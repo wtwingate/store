9a0694e20c45ad3d3e368f2771ea6acdb183f0c7
William Wingate <wtwingate@gmail.com>
2025-10-17 14:45:40 -0400

    Allow users to change their email address

--- config/routes.rb
+++ config/routes.rb
@@ -23,10 +23,15 @@ Rails.application.routes.draw do
   resource :sign_up
 
   namespace :settings do
+    resource :email, only: [ :show, :update ]
     resource :password, only: [ :show, :update ]
     resource :profile, only: [ :show, :update ]
     resource :user, only: [ :show, :destroy ]
 
     root to: redirect("/settings/profile")
+  end
+
+  namespace :email do
+    resources :confirmations, param: :token, only: [ :show ]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_27_180515) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_17_175837) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -80,6 +80,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_27_180515) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "unconfirmed_email"
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
