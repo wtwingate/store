9b9ea2544f5776fec18bcb68c81b2c4835425cfd
William Wingate <wtwingate@gmail.com>
2026-05-07 22:59:18 -0400

    Bump Rails to 8.1.3

--- db/schema.rb
+++ db/schema.rb
@@ -10,36 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_06_152922) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_08_025309) do
   create_table "action_text_rich_texts", force: :cascade do |t|
-    t.string "name", null: false
     t.text "body"
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
     t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.bigint "record_id", null: false
+    t.string "record_type", null: false
     t.datetime "updated_at", null: false
     t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
   end
 
   create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.bigint "record_id", null: false
+    t.string "record_type", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
     t.bigint "byte_size", null: false
     t.string "checksum"
+    t.string "content_type"
     t.datetime "created_at", null: false
+    t.string "filename", null: false
+    t.string "key", null: false
+    t.text "metadata"
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
@@ -50,58 +50,58 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_06_152922) do
   end
 
   create_table "products", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.integer "inventory_count"
+    t.string "name"
+    t.datetime "updated_at", null: false
     t.integer "wishlists_count", default: 0
   end
 
   create_table "sessions", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "ip_address"
-    t.string "user_agent"
     t.datetime "created_at", null: false
+    t.string "ip_address"
     t.datetime "updated_at", null: false
+    t.string "user_agent"
+    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
   create_table "subscribers", force: :cascade do |t|
-    t.integer "product_id", null: false
-    t.string "email"
     t.datetime "created_at", null: false
+    t.string "email"
+    t.integer "product_id", null: false
     t.datetime "updated_at", null: false
     t.index ["product_id"], name: "index_subscribers_on_product_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email_address", null: false
-    t.string "password_digest", null: false
+    t.boolean "admin"
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "email_address", null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "password_digest", null: false
     t.string "unconfirmed_email"
-    t.boolean "admin"
+    t.datetime "updated_at", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
   create_table "wishlist_products", force: :cascade do |t|
-    t.integer "product_id", null: false
-    t.integer "wishlist_id", null: false
     t.datetime "created_at", null: false
+    t.integer "product_id", null: false
     t.datetime "updated_at", null: false
+    t.integer "wishlist_id", null: false
     t.index ["product_id", "wishlist_id"], name: "index_wishlist_products_on_product_id_and_wishlist_id", unique: true
     t.index ["product_id"], name: "index_wishlist_products_on_product_id"
     t.index ["wishlist_id"], name: "index_wishlist_products_on_wishlist_id"
   end
 
   create_table "wishlists", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
     t.string "name"
     t.integer "products_count", default: 0
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_wishlists_on_user_id"
   end
 
